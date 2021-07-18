236e3f8bbaa1183a77814a1497213b348c00f614
n <nnizovkin>
2021-07-18 17:40:53 +0300

    Initial commit

---
+++ src/ru/test/ShortestPath.java
@@ -0,0 +1,18 @@
+package ru.test;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ShortestPath {
+    public static void main(String[] args) throws IOException {
+        String fileName = args[0];
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+//            double size = Double.parseDouble();
+//            String line;
+//            while ((line = br.readLine()) != null) {
+//                // process the line.
+//            }
+        }
+    }
+}
